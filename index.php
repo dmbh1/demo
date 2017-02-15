705ae8c5ce63af0ab5ed98e5011a0e9ff896a99a
dmbh1 <dave@dmbh.co.uk>
2017-02-15 22:33:48 +0000

    baseline

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>DMBH web site and content support</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- meta name="description" content="PX Wheels offer a like for like, alloy wheel exchange service. We send you a set of refubished wheels, you send us your old wheels back. Easy" -->
+    <!-- meta name="keywords" content="alloys, alloy, wheel, wheels, part, exchange, part-exchange, blackpool, refurbished, refurbishing, reconditioned" -->
+
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/test3_pxwheels.css" />
+    </script><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script>var __adobewebfontsappname__="dreamweaver"</script>
+    <script src="http://use.edgefonts.net/raleway:n5,n2,n1,n4:default.js" type="text/javascript"></script>
+    <!-- Bootstrap overrides, should be in a css included after the bootstrap css so they override where needed -->
+</head>
+<body>
+
+<!-- Standard Header -->
+<?php include 'include/navbar.html' ?>
+
+<div class="fluid-container">
+    <div class="well well-no-bot-margin">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="panel panel-info">
+                    <!-- <div class="panel-heading">Welcome to pxwheels (this is just to show panel header)</div> -->
+                    <div class="panel-body main-title">
+                        <h2 class="main-title-text">Hello</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </div>
+</div>
+
+
+
+
+<?php include 'include/footer.html' ?>
+
+
+</body>
+</html>
